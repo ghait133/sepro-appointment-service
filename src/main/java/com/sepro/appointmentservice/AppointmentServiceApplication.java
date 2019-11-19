3dd8875de75a20b1330643e4656b9a0e195a79f1
Ghait <ghait@mogic.com>
2019-11-19 07:32:47 +0100

    adding swagger

--- src/main/java/com/sepro/appointmentservice/AppointmentServiceApplication.java
+++ src/main/java/com/sepro/appointmentservice/AppointmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.sepro.appointmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
